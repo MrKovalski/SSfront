a21b399ee7993f5b44d24769dfe8207aeb6162ad
Filip Ilic <info@filipilic.info>
2019-06-19 12:10:13 +0200

    dodate osnovne komponente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { StudentViewComponent } from './student-view/student-view.component';
+import { AdminViewComponent } from './admin-view/admin-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    StudentViewComponent,
+    AdminViewComponent
   ],
   imports: [
     BrowserModule,
